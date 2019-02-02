54a21f0071e6beeb02f3bb5f4fe8437aebbc63d0
Jose Hernandez <jose.hernandeztorres@yahoo.com>
2019-02-01 18:50:17 -0700

    Logic to find best match in progress

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,32 @@ module.exports = function(app){
     })
 // API POST Requests
     app.post("/api/friends", (req, res) => {
-        friendData.push(req.body);
-        res.json(friendData);
+        let userScore = req.body.scores;
+
+        let friendName = '';
+        let friendImg = '';
+        let totalDiff = 50;
+
+        // Looping through friends array
+        for(let i = 0; i < friendData.length - 1; i++){
+            let diff = 0;
+            // Looping through score array from user input
+            for(let j = 0; j < userScore.length; j++){
+                diff += Math.abs(friendData[i].scores[j] - userScore[j]);
+            }
+
+            if(diff <= totalDiff){
+                console.log(`Closest match found = ${diff}
+Friend name = ${friendData[i].name}
+Friend image = ${friendData[i].photo}`);
+            }
+        }
+
+        // friendData.push();
+
+
+
+        // friendData.push(req.body);
+        res.json({status: 'OK', friendName: friendName, friendImg: friendImg});
     });
 };
